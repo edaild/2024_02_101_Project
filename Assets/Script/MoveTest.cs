25aba5419ecdb05f7b598c4319ee762e54a96069
YoonHo Jeong <109999749+edaild@users.noreply.github.com>
2024-09-04 10:17:40 +0900

    Update MoveTest.cs
    플레이어 이동 변수 선언

--- Assets/Script/MoveTest.cs
+++ Assets/Script/MoveTest.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveTest : MonoBehaviour
-{
+{    
+    public Transform Player;
     // Start is called before the first frame update
     void Start()
     {
